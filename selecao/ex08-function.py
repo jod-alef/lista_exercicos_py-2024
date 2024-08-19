0b4d4a36151ca2e2866bc330e3528fe255a059da
Jod Alef <jod.alef@gmail.com>
2024-08-19 19:54:49 -0300

    melhoria do código ex 08-function

--- selecao/ex08-function.py
+++ selecao/ex08-function.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 #8) Escreva um programa para ler o ano de nascimento de uma pessoa e escrever uma mensagem que
 # diga se ela poderá ou não votar este ano (não é necessário considerar o mês em que ela nasceu).
 
@@ -5,7 +7,7 @@ print("Verificador de idade para participar de votação")
 idade = int(input("Digite o ano que você nasceu com 4 digitos (ex: 1992): "))
 
 def verificarVoto():
-    if 2024 - idade < 16:
+    if date.today().year - idade < 16:
         naovota = 16 - (2024 - idade)
         return("Você ainda não pode voltar, faltam", naovota, "anos")
     else:
