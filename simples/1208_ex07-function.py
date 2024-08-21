113ea79d0a6b8fd59e9137fe33fcc108bfa2525f
Jod Alef <jod.alef@gmail.com>
2024-08-21 19:52:45 -0300

    Adição de ex07 - função

---
+++ simples/1208_ex07-function.py
@@ -0,0 +1,18 @@
+#Exercicio 7
+#A equipe Benneton-Ford deseja calcular o número mínimo de litros que deverá colocar no tanque de
+#seu carro para que ele possa percorrer um determinado número de voltas até o primeiro
+#reabastecimento. Escreva um programa que leia o comprimento da pista (em metros), o número total de
+#voltas a serem percorridas no grande prêmio, o número de abastecimentos desejados e o consumo de
+#combustível do carro (em Km/L). Calcular e escrever o número mínimo de litros necessários para
+#percorrer até o primeiro reabastecimento. OBS: Considere que o número de voltas entre os
+#abastecimentos é o mesmo.
+
+comprimentoPista = float(input("Insira o comprimento total da pista: "))
+numeroVoltas = int(input("Insira o número de voltas a serem percorridas: "))
+numeroAbastecimento = int(input("Insira o número de abastecimentos desejado: "))
+consumoVeiculo = float(input("Qual o consumo médio do carro em Km/l? "))
+
+def calculoLitros(comprimentoPista, numeroVoltas, consumoVeiculo, numeroAbastecimento):
+    return (((comprimentoPista / 1000) * numeroVoltas)/consumoVeiculo)/numeroAbastecimento
+
+print("Numero mínimo de litros necessário para percorrer até abastecimento", calculoLitros(comprimentoPista,numeroVoltas,numeroAbastecimento,consumoVeiculo))
