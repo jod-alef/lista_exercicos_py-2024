4be616da1cbe06f15285770eb402720d36e1f144
Jod Alef <jod.alef@gmail.com>
2024-08-21 18:32:15 -0300

    Envio exercicios 3 e 4

---
+++ selecao/ex03e04.py
@@ -0,0 +1,19 @@
+''' 
+3) Escreva um programa para ler as notas das duas avaliações de um aluno no semestre, calcular e
+escrever a média semestral e a seguinte mensagem: PARABÉNS! Você foi aprovado! somente se o
+aluno foi aprovado (considere 6.0 a média mínima para aprovação).
+4) Acrescente ao exercício anterior a mensagem Você foi REPROVADO! Estude mais... caso a
+média calculada seja menor que 6.0. 
+'''
+
+def entradaNotas():
+    nota1 = float(input("Digite a primeira nota: "))
+    nota2 = float(input("Digite a segunda nota: "))
+    media = (nota1 + nota2)/2
+    
+    if media < 6.0:
+        print("Você foi REPROVADO!")
+    else:
+        print("PARABÉNS! Você foi aprovado!")
+
+entradaNotas()
