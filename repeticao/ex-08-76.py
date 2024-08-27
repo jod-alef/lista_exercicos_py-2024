5c35c440538c0a7bd8adf3d1f9de7324231e4fc6
Jod Alef <jod.alef@gmail.com>
2024-08-27 10:24:38 -0300

    envio de exercicio 08-76

---
+++ repeticao/ex-08-76.py
@@ -0,0 +1,18 @@
+import time
+
+print("Bem vind@, este é um algoritimo que lê 50 valores inteiros positivos e:")
+print("* Encontra o maior e o menor valor.")
+print("* Calcula a media dos números lidos.")
+time.sleep(1.5)
+quantidadeNumero = int(input("Quantos números você quer adicionar? "))
+
+listaNumeros = []
+for i in range(quantidadeNumero):
+    numeros = int(input("Insira numero: "))
+    listaNumeros.append(numeros)
+
+listaNumeros.sort()
+print(f"O menor número é", listaNumeros[0])
+print(f"O menor número é", listaNumeros[-1])
+print(f"A soma de todos os números é", sum(listaNumeros))
+
