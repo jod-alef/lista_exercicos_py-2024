5963f8e0a207aa91f45b058609615eabb602b825
Jod Alef <jod.alef@gmail.com>
2024-08-27 10:37:04 -0300

    adição de time.sleep para melhoria de UX

--- repeticao/ex-08-76.py
+++ repeticao/ex-08-76.py
@@ -1,7 +1,9 @@
 import time
 
 print("Bem vind@, este é um algoritimo que lê 50 valores inteiros positivos e:")
+time.sleep(0.5)
 print("* Encontra o maior e o menor valor.")
+time.sleep(0.5)
 print("* Calcula a media dos números lidos.")
 time.sleep(1.5)
 quantidadeNumero = int(input("Quantos números você quer adicionar? "))
@@ -15,4 +17,3 @@ listaNumeros.sort()
 print(f"O menor número é", listaNumeros[0])
 print(f"O menor número é", listaNumeros[-1])
 print(f"A soma de todos os números é", sum(listaNumeros))
-
